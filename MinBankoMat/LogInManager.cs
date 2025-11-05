62f7ded1ee0649747901883fa91761db8109292a
erik ny <erik.nyy@gmail.com>
2025-11-05 14:47:10 +0100

    Created login method

---
+++ MinBankoMat/LogInManager.cs
@@ -0,0 +1,31 @@
+﻿namespace MinBankoMat
+{
+   internal class LogInManager
+   {
+      static List<User> users = new List<User>();
+      public User LogIn()
+      {
+         bool running = true;
+         while (running)
+         {
+            Console.Write("Ange ditt användarnamn:");
+            string userName = Inputs.GetString();
+            Console.Write("Ange din pinkod:");
+            int pinCode = Inputs.GetUserNumber();
+
+            foreach (var user in users)
+            {
+               if (user.UserName == userName && user.PinCode == pinCode)
+               {
+                  Console.WriteLine("Lyckad inloggning");
+                  return user;
+                  
+               }
+            }
+            Console.WriteLine("Ogiltigt användarnamn/lösenord");
+         }
+
+         return null;
+      }
+   }
+}
--- MinBankoMat/User.cs
+++ MinBankoMat/User.cs
@@ -8,6 +8,7 @@ namespace MinBankoMat
 {
    internal class User
    {
+      
       public string UserName { get; set; } = string.Empty;
       public int PinCode { get; set; }
       public decimal Balance { get; set; }
